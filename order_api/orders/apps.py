4559835a479d3dcd0a2e70ebd0e260ee2b35c0cd
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2025-08-26 16:58:27 +0500

    Add initial migrations for Account and Order models

--- order_api/orders/apps.py
+++ order_api/orders/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class OrdersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "order_api.orders"
+
+    def ready(self):
+        from . import signals  # noqa: F401, PLC0415
